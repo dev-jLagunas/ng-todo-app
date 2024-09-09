cdc2a9414a7b892b3bdfa854eda3d2864072dc1a
Juan Lagunas <dev.jlagunas@gmail.com>
2024-09-10 08:11:51 +0900

    feat(Todo Input Component): set up input html, add styles, and add to U.I.

---
+++ src/app/todo-filter/todo-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-filter.component.html',
+  styleUrl: './todo-filter.component.css'
+})
+export class TodoFilterComponent {
+
+}
--- src/app/todo-main/todo-main.component.ts
+++ src/app/todo-main/todo-main.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { TodoHeaderComponent } from '../todo-header/todo-header.component';
+import { TodoInputComponent } from '../todo-input/todo-input.component';
+import { TodoListComponent } from '../todo-list/todo-list.component';
+import { TodoFilterComponent } from '../todo-filter/todo-filter.component';
 
 @Component({
   selector: 'app-todo-main',
   standalone: true,
-  imports: [TodoHeaderComponent],
+  imports: [
+    TodoHeaderComponent,
+    TodoInputComponent,
+    TodoListComponent,
+    TodoFilterComponent,
+  ],
   templateUrl: './todo-main.component.html',
   styleUrl: './todo-main.component.css',
 })
